ac7cfa36d00bd9d5a9bbfa204164e1921772d756
unknown <yilwu@PBB-ITC-C06.iowa.uiowa.edu>
2013-02-23 16:17:05 -0600

    done page and function

--- index.aspx
+++ index.aspx
@@ -7,21 +7,40 @@
     <title></title>
 </head>
 <body>
+    <form id="form1" runat="server">
+<div>
+<h1>Salary Calculator</h1>
+</div>
+    
+<div>
+Hourly Wage:
+    <br />
+    <asp:TextBox ID="wageTextBox" runat="server"></asp:TextBox>
+    <br />
+    <br />
+    Hours Work:
+    <br />
+    <asp:TextBox ID="hourTextbox" runat="server"></asp:TextBox>
+    <br />
+    <br />
+    Pre-tax Deduction:
+    <br />
+    <asp:TextBox ID="preTaxTextBox" runat="server"></asp:TextBox>
+    <br />
+    <br />
+    After-tax Deduction:
+    <br />
+    <asp:TextBox ID="aftTaxTextBox" runat="server"></asp:TextBox>
+    <br />
+    <br />
+    <asp:Button ID="clearButton" runat="server" Text="Clear" />
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:Button ID="calButton" runat="server" Text="Calculate" />
+    <br />
+    <br />
+    <asp:Label ID="resultLabel" runat="server"></asp:Label>
 
- 
-           
-
-    <p>
-        &nbsp;</p>
-    <p>
-        &nbsp;</p>
-    <p>
-        &nbsp;</p>
-    <p>
-        &nbsp;</p>
-
- 
-           
-
+</div>
+    </form>
 </body>
 </html>
--- index.aspx.vb
+++ index.aspx.vb
@@ -1,8 +1,30 @@
 ﻿
 Partial Class index
     Inherits System.Web.UI.Page
-
-    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-
+    Protected Sub clearButton_Click(sender As Object, e As System.EventArgs) Handles clearButton.Click
+        preTaxTextBox.Text = ""
+        aftTaxTextBox.Text = ""
+        hourTextbox.Text = ""
+        wageTextBox.Text = ""
+        resultLabel.Text = ""
     End Sub
+
+    Protected Sub calButton_Click(sender As Object, e As System.EventArgs) Handles calButton.Click
+        Dim workhour As Decimal = hourTextbox.Text 
+        Dim wage As Decimal = wageTextBox.Text
+        Dim preTaxDeduction As Decimal = preTaxTextBox.Text
+        Dim afterTaxDeduction As Decimal = aftTaxTextBox.Text
+        Dim netPay As Decimal
+        Dim deductedTax As Decimal
+
+        deductedTax = ((workhour * wage - preTaxDeduction) * 0.18 - afterTaxDeduction)
+        netPay = (workhour * wage) - deductedTax
+
+        resultLabel.Text = "Tax is evil!!! You got $" & deductedTax & " Deducted. And You Net Payment is $" & netPay
+
+
+        On Error Resume Next
+    End Sub
+
+
 End Class
